8c53f64eb2a44c9760b065f6a16c0494f8561999
ObedLandu <obedlandu08@gmail.com>
2025-09-04 21:28:35 +0100

    new commit

--- firstProjectReact/src/App.jsx
+++ firstProjectReact/src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Superhero from "./components/Superhero/Superhero";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <main>
+      <h1>Marvel</h1>
+      <div className="superheroes">
+        {/* superhero numéro 1 */}
+        <Superhero />
 
-export default App
+        {/* superhero numéro 2 */}
+        <div className="superhero">
+          <h2>Captain America</h2>
+          <p>
+            Captain America is a comic-strip superhero created by writer Joe
+            Simon and artist Jack Kirby for Timely (later Marvel) Comics. The
+            character debuted in March 1941 in Captain America Comics no. 1. The
+            star-spangled super soldier would become one of Marvel’s most iconic
+            and enduring characters.
+          </p>
+        </div>
+
+        {/* superhero numéro 3 */}
+        <div className="superhero">
+          <h2>Superman</h2>
+          <p>
+            Superman is a fictional superhero appearing in American comic books
+            published by DC Comics. Created by writer Jerry Siegel and artist
+            Joe Shuster, the character first appeared in Action Comics #1 (June
+            1938). Superman is the alter ego of Clark Kent, a mild-mannered
+            reporter for the Daily Planet in the city of Metropolis. Born on the
+            planet Krypton as Kal-El, he was sent to Earth as an infant by his
+            parents just before Krypton's destruction. Raised by a Kansas
+            farming couple, he developed superhuman abilities under Earth's
+            yellow sun, including super strength, flight, invulnerability, heat
+            vision, x-ray vision, super speed, and enhanced senses.
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
---
+++ firstProjectReact/src/components/Superhero/Superhero.jsx
@@ -0,0 +1,17 @@
+export default function Superhero () {
+    return (
+        <div className="superhero">
+          <h2>Hulk</h2>
+          <p>
+            Hulk is a fictional superhero appearing in American comic books
+            published by Marvel Comics. Created by writer Stan Lee and artist
+            Jack Kirby, the character first appeared in The Incredible Hulk #1
+            (May 1962). The Hulk is the alter ego of Dr. Robert Bruce Banner, a
+            physically weak, socially withdrawn, and emotionally reserved
+            physicist who, after a gamma bomb test goes wrong, transforms into a
+            raging green-skinned giant whenever he is subjected to emotional
+            stress.
+          </p>
+        </div>
+    )
+}
