2754521f5db64243b22e499646402d4b886159a6
ObedLandu <obedlandu08@gmail.com>
2025-09-05 10:18:29 +0100

    props

--- firstProjectReact/src/App.jsx
+++ firstProjectReact/src/App.jsx
@@ -7,25 +7,33 @@ export default function App() {
       <h1>Marvel</h1>
       <div className="superheroes">
         {/* superhero numéro 1 */}
-        <Superhero />
+        <Superhero
+          name="Hulk"
+          description="Hulk is a fictional superhero appearing in American comic books
+            published by Marvel Comics. Created by writer Stan Lee and artist
+            Jack Kirby, the character first appeared in The Incredible Hulk #1
+            (May 1962). The Hulk is the alter ego of Dr. Robert Bruce Banner, a
+            physically weak, socially withdrawn, and emotionally reserved
+            physicist who, after a gamma bomb test goes wrong, transforms into a
+            raging green-skinned giant whenever he is subjected to emotional
+            stress."
+            films={["Avengers", "Age of Ultron", "Thor: Ragnarok"]}
+        />
 
         {/* superhero numéro 2 */}
-        <div className="superhero">
-          <h2>Captain America</h2>
-          <p>
-            Captain America is a comic-strip superhero created by writer Joe
+          <Superhero
+            name="Captain America"
+            description=" Captain America is a comic-strip superhero created by writer Joe
             Simon and artist Jack Kirby for Timely (later Marvel) Comics. The
             character debuted in March 1941 in Captain America Comics no. 1. The
             star-spangled super soldier would become one of Marvel’s most iconic
-            and enduring characters.
-          </p>
-        </div>
+            and enduring characters."
+          />
 
         {/* superhero numéro 3 */}
-        <div className="superhero">
-          <h2>Superman</h2>
-          <p>
-            Superman is a fictional superhero appearing in American comic books
+          <Superhero
+            name="Superman"
+            description=" Superman is a fictional superhero appearing in American comic books
             published by DC Comics. Created by writer Jerry Siegel and artist
             Joe Shuster, the character first appeared in Action Comics #1 (June
             1938). Superman is the alter ego of Clark Kent, a mild-mannered
@@ -34,9 +42,8 @@ export default function App() {
             parents just before Krypton's destruction. Raised by a Kansas
             farming couple, he developed superhuman abilities under Earth's
             yellow sun, including super strength, flight, invulnerability, heat
-            vision, x-ray vision, super speed, and enhanced senses.
-          </p>
-        </div>
+            vision, x-ray vision, super speed, and enhanced senses."
+          />
       </div>
     </main>
   );
--- firstProjectReact/src/components/Superhero/Superhero.jsx
+++ firstProjectReact/src/components/Superhero/Superhero.jsx
@@ -1,17 +1,21 @@
-export default function Superhero () {
-    return (
-        <div className="superhero">
-          <h2>Hulk</h2>
-          <p>
-            Hulk is a fictional superhero appearing in American comic books
-            published by Marvel Comics. Created by writer Stan Lee and artist
-            Jack Kirby, the character first appeared in The Incredible Hulk #1
-            (May 1962). The Hulk is the alter ego of Dr. Robert Bruce Banner, a
-            physically weak, socially withdrawn, and emotionally reserved
-            physicist who, after a gamma bomb test goes wrong, transforms into a
-            raging green-skinned giant whenever he is subjected to emotional
-            stress.
-          </p>
-        </div>
-    )
+export default function Superhero({
+  name,
+  description,
+  films = ["Aucun film pour ce superHero"],
+  ...props
+}) {
+  return (
+    <div className="superhero">
+      <h2> {name} </h2>
+      <p>{description}</p>
+      <div>
+        <b>Films :</b>
+        <ul>
+          {films.map((film) => (
+            <li>{film}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
